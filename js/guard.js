0614b4bef43d872c9ca975288c0ff7dbec4539d4
Lasha Abramishvili <abramishvililasha25@gmail.com>
2026-07-24 20:00:32 +0400

    Add authentication guard to protected pages

--- js/guard.js
+++ js/guard.js
@@ -1,1 +1,19 @@
-// This file will contain authentication guard logic.
+function getSession() {
+  const savedSession = localStorage.getItem("crm_session");
+
+  if (savedSession) {
+    return JSON.parse(savedSession);
+  }
+
+  return null;
+}
+
+function requireAuth() {
+  const session = getSession();
+
+  if (!session) {
+    window.location.href = "index.html";
+  }
+}
+
+requireAuth();
